4af110f0fa0d0081e6a80662ec85c01eb116731a
TulioCapriles <tulio.capriles@kellogg.northwestern.edu>
2021-02-02 22:10:23 +0100

    a

--- lab/week4.js
+++ lab/week4.js
@@ -16,9 +16,30 @@ async function pageLoaded() {
   let response = await fetch('https://kiei451.com/api/products.json')
   let json = await response.json()
 
-  // writes the returned JSON to the console
   console.log(json)
-  
+
+  for(i=0; i<json.products.lenght; i++) {
+    let productName = json.products[i].name
+    let productImage = json.products[i].image
+    let productPrice = json.products[i].price
+    let productDesc = json.products[i].description
+
+    let outputElement = document.querySelector('.products')
+    outputElement.insertAdjacentHTML('beforeend',
+
+      `<div class="p-4 w-full md:w-1/2 lg:w-1/3">
+        <div class="border h-full p-4 flex flex-col">
+          <h2 class="text-lg font-bold mb-4">${productName}</h2>
+          <div class="mb-4"><img src="${productImage}">
+          </div>
+          <div class="mb-4 text-gray-900">
+          ${productDesc}
+          </div>
+          <div class="mt-auto text-purple-500 text-2xl">${productPrice}</div>
+        </div>
+      </div>`
+    )
+  }
   // 🔥 start here: write code to loop through the products; each
   // iteration of the loop should call your renderProduct function
   // make it work first; then extract to the separate renderProduct
